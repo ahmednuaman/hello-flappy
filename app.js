3114cf2c444c3c386ed3c83fc2dd60f369a31921
Ahmed Nuaman <ahmed@ahmednuaman.com>
2014-02-17 20:06:12 +0000

    added flappy bird

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 (function (document, PIXI) {
-  var renderer = new PIXI.autoDetectRenderer(432, 768),
+  var birdCounter = 0,
+      renderer = new PIXI.autoDetectRenderer(432, 768),
       spriteBg1 = new PIXI.Sprite(PIXI.Texture.fromImage('images/bg.png')),
       spriteBg2 = new PIXI.Sprite(PIXI.Texture.fromImage('images/bg.png')),
-      stage = new PIXI.Stage(0xffffff);
+      stage = new PIXI.Stage(0xffffff),
+      mcBird;
 
   document.body.appendChild(renderer.view);
 
@@ -23,6 +25,26 @@
     }
   }
 
+  function addBird () {
+    var spriteBird0 = PIXI.Texture.fromImage('images/flap0.png'),
+        spriteBird1 = PIXI.Texture.fromImage('images/flap1.png'),
+        spriteBird2 = PIXI.Texture.fromImage('images/flap2.png');
+
+    mcBird = new PIXI.MovieClip([spriteBird0, spriteBird1, spriteBird2]);
+
+    mcBird.x = 20;
+
+    stage.addChild(mcBird);
+  }
+
+  function animateBirdFlap () {
+    mcBird.gotoAndStop(birdCounter++);
+
+    if (birdCounter > 2) {
+      birdCounter = 0;
+    }
+  }
+
   function animate () {
     animateBg();
 
@@ -31,5 +53,9 @@
   }
 
   addBg();
+  addBird();
+
   animate();
+
+  setInterval(animateBirdFlap, 100);
 })(document, PIXI);
