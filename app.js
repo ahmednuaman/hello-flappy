fe795191b0ce9511d3482c56eaf992b346c4c0d1
Ahmed Nuaman <ahmed@ahmednuaman.com>
2014-02-18 20:38:36 +0000

    added pipes

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
 (function (document, PIXI, TweenMax, Ease) {
-  var birdCounter = 0,
+  var appHeight = 768,
+      appWidth = 432,
+      birdCounter = 0,
       radDown = Math.PI * .25,
-      radUp = -radDown,
-      renderer = new PIXI.autoDetectRenderer(432, 768),
+      radUp = 0,
+      pipeDown0 = new PIXI.Sprite(PIXI.Texture.fromImage('images/pipe-down.png')),
+      pipeDown1 = new PIXI.Sprite(PIXI.Texture.fromImage('images/pipe-down.png')),
+      pipeUp0 = new PIXI.Sprite(PIXI.Texture.fromImage('images/pipe-up.png')),
+      pipeUp1 = new PIXI.Sprite(PIXI.Texture.fromImage('images/pipe-up.png')),
+      pipeYMax = 360,
+      pipeYMin = 45,
+      renderer = new PIXI.autoDetectRenderer(appWidth, appHeight),
+      spriteBg0 = new PIXI.Sprite(PIXI.Texture.fromImage('images/bg.png')),
       spriteBg1 = new PIXI.Sprite(PIXI.Texture.fromImage('images/bg.png')),
-      spriteBg2 = new PIXI.Sprite(PIXI.Texture.fromImage('images/bg.png')),
       stage = new PIXI.Stage(0xffffff),
-      mcBird;
+      mcBird,
+      pipes,
+      pipesLength;
 
   document.body.appendChild(renderer.view);
 
   function addBg () {
+    stage.addChild(spriteBg0);
     stage.addChild(spriteBg1);
-    stage.addChild(spriteBg2);
 
-    spriteBg2.x = spriteBg2.width;
+    spriteBg1.x = spriteBg1.width;
   }
 
   function animateBg () {
+    spriteBg0.x--;
     spriteBg1.x--;
-    spriteBg2.x--;
 
-    if (spriteBg2.x === 0) {
-      spriteBg1.x = 0
-      spriteBg2.x = spriteBg2.width;
+    if (spriteBg1.x === 0) {
+      spriteBg0.x = 0
+      spriteBg1.x = spriteBg1.width;
     }
   }
 
@@ -47,7 +57,7 @@
   }
 
   function animateBirdFall () {
-    TweenMax.to(mcBird, 1.5, {
+    TweenMax.to(mcBird, 2, {
       y: 768,
       rotation: radDown,
       ease: Ease.easeIn,
@@ -57,8 +67,8 @@
 
   function flapBird () {
     TweenMax.killTweensOf(mcBird);
-    TweenMax.to(mcBird, .25, {
-      y: '-=' + 100,
+    TweenMax.to(mcBird, .3, {
+      y: '-=' + 50,
       rotation: radUp,
       ease: Ease.easeOut,
       onUpdate: limitBird,
@@ -82,14 +92,68 @@
     animateBirdFall();
   }
 
+  function addPipes () {
+    stage.addChild(pipeDown0);
+    stage.addChild(pipeDown1);
+    stage.addChild(pipeUp0);
+    stage.addChild(pipeUp1);
+
+    pipeDown0.x = pipeUp0.x = appWidth;
+    pipeDown1.x = pipeUp1.x = appWidth * 1.5;
+
+    setRandomYForUpPipe(pipeUp0);
+    setRandomYForUpPipe(pipeUp1);
+    setRandomYForDownPipe(pipeDown0);
+    setRandomYForDownPipe(pipeDown1);
+
+    pipes = [pipeDown0, pipeDown1, pipeUp0, pipeUp1];
+    pipesLength = pipes.length - 1;
+  }
+
+  function setRandomYForUpPipe (pipe) {
+    pipe.y = getRandomNumber(appHeight - pipeYMax, appHeight - pipeYMin);
+  }
+
+  function setRandomYForDownPipe (pipe) {
+    pipe.y = getRandomNumber(-pipeYMin, -pipeYMax);
+  }
+
+  function getRandomNumber(min, max) {
+    console.log(min, max);
+    return Math.random() * (max - min) + min;
+  }
+
+  function animatePipes () {
+    var i,
+        pipe;
+
+    for (i = pipesLength; i >= 0; i--) {
+      pipe = pipes[i];
+
+      pipe.x--;
+
+      if (pipe.x < -pipe.width) {
+        pipe.x = appWidth;
+
+        if (pipe === pipeUp0 || pipe === pipeUp1) {
+          setRandomYForUpPipe(pipe);
+        } else {
+          setRandomYForDownPipe(pipe);
+        }
+      }
+    };
+  }
+
   function animate () {
     animateBg();
+    animatePipes();
 
     renderer.render(stage);
     requestAnimFrame(animate);
   }
 
   addBg();
+  addPipes();
   addBird();
   animateBirdFall();
 
